820caea230f0ed5f29b4f4b854f31921aa3dee70
camilly2020 <61083157+camilly2020@users.noreply.github.com>
2020-09-09 13:39:11 -0300

    exercicio 2

---
+++ ifpb/bmi.js
@@ -0,0 +1,20 @@
+peso  const =  200 ;
+altura  const =  1,5 ;
+const  bmi  =  peso / altura ** 2
+
+deixe o  resultado
+
+if  ( bmi <= 18 , 5 ) {
+    resultado  =  "Peso insuficiente" ;
+}
+else  if  ( bmi <= 24 , 9 ) {
+    resultado  =  "Peso normal" ;
+}
+else  if  ( bmi <= 29 , 9 ) {
+    resultado  =  "Sobrepeso" ;
+}
+else {
+    resultado  =  "Obesidade" ;
+}
+
+console . log  ( `IMC: $ { resultado } ` ) ;
