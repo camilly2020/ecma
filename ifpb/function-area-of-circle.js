e2f322a0cbb3d6a9698849aca9844f9ee2d1bbf1
camilly2020 <61083157+camilly2020@users.noreply.github.com>
2020-09-09 14:18:41 -0300

    exercicio 4

---
+++ ifpb/function-area-of-circle.js
@@ -0,0 +1,7 @@
+const  pi  =  Math . PI ;
+function  AreaCirculo ( raio ) {
+   área  var =  pi * raio ** 2 ;
+  console . log ( "A área do Círculo é:" , área ) ;
+} ;
+var  Raio  =  10 ;
+AreaCirculo ( Raio ) 
