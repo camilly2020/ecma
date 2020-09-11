d75d2c7b7762dc154786071e5a621846fc5cc5ae
camilly2020 <61083157+camilly2020@users.noreply.github.com>
2020-09-11 11:01:01 -0300

    exercicio 4

---
+++ ifpb/area-of-circle.js
@@ -0,0 +1,3 @@
+let raio = 10;
+let Area = Math.PI * raio ** 2;
+console.log(Area);
--- ifpb/function-area-of-circle.js
+++
@@ -1,6 +0,0 @@
-function Area(raio) {
-  let AreaCirculo;
-  AreaCirculo = Math.PI * raio ** 2;
-  return AreaCirculo;
-}
-module.export = Area;
