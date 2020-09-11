7cd4a1527d3ccb22788a6e346f9741b95bcb9604
camilly2020 <61083157+camilly2020@users.noreply.github.com>
2020-09-11 10:42:36 -0300

    exercicio 2

--- ifpb/bmi.js
+++ ifpb/bmi.js
@@ -1,19 +1,22 @@
+
 const peso =  200 ;
 const altura  =  1.5 ;
 const  bmi  =  peso / altura ** 2;
-let result;
+let calculo;
+
 
 if  ( bmi <= 18.5 ){
-    result  =  "Peso insuficiente";
+    calculo  =  "Peso insuficiente";
 }
 else  if  ( bmi <= 24.9 ) {
-    result  =  "Peso normal";
+    calculo =  "Peso normal";
 }
 else  if  ( bmi <= 29.9 ) {
-    result  =  "Sobrepeso" ;
+    calculo =  "Sobrepeso" 
+
 }
 else {
-    result  =  "Obesidade";
+    calculo  =  "Obesidade";
 }
 
 console.log  ( `IMC: $ { resultado } ` ) ;
