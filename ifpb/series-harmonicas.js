45036a39f1f16873e8a2043c903e90ceb3e5189f
camilly2020 <61083157+camilly2020@users.noreply.github.com>
2020-09-11 08:26:11 -0300

    exercicio 3

--- ifpb/series-harmonicas.js
+++ ifpb/series-harmonicas.js
@@ -1,6 +1,6 @@
-deixe  a  =  10 ; 
-deixe  soma  =  0 ;
-para ( deixe  i  =  1 ;  i <= a ;  i ++ ) {
+n const  =  10 ; 
+deixe soma  =  0 ;
+para ( deixe  i  =  1 ;  i <= n ;  i +=) {
   soma  + =  1 / i
 }
 console . log ( soma )
