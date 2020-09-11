508bac83276ad5682997d2166e6eb3de7b0d9de5
camilly2020 <61083157+camilly2020@users.noreply.github.com>
2020-09-11 08:56:10 -0300

    exercicio 5

--- ifpb/node-hello.js
+++ ifpb/node-hello.js
@@ -1,14 +1,8 @@
-const http = require('http');
+var http = require("http");
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+http
+  .createServer(function(req, res) {
+    res.write("Hello World!"); 
+    res.end(); 
+  })
+  .listen(8080);
